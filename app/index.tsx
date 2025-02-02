807420e3bc2f0fc864b3964877daa7389f9401ac
razvanmc15 <razvanmc15@gmail.com>
2025-02-02 19:33:01 +0200

    fresh app start

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-500 h-full">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
