e67e5d5897a5fcdc044c4373bf105dfb61f89f9a
razvanmc15 <razvanmc15@gmail.com>
2025-02-10 06:42:23 +0200

    1% progress

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -5,12 +5,20 @@ import {
 } from "@react-navigation/drawer";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { router } from "expo-router";
 
 // Define the props type for CustomDrawerContent explicitly
 function CustomDrawerContent(props: DrawerContentComponentProps) {
   return (
     <DrawerContentScrollView {...props}>
-      <DrawerItem label="Home" onPress={() => {}} />
+      <DrawerItem
+        label="Habits"
+        onPress={() => router.push("/(drawer)/(tabs)/(habits)")}
+        icon={({ color, size }) => (
+          <AntDesign name="profile" size={24} color="black" />
+        )}
+      />
     </DrawerContentScrollView>
   );
 }
