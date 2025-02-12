2bb171d629d8b63e7009baf4d3a8808724de95bb
razvanmc15 <razvanmc15@gmail.com>
2025-02-12 19:22:55 +0200

    progress 1% daily...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,33 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "../global.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Manrope-Bold": require("../assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-Medium": require("../assets/fonts/Manrope-Medium.ttf"),
+    "Manrope-Regular": require("../assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-SemiBold": require("../assets/fonts/Manrope-SemiBold.ttf"),
+    "Manrope-ExtraBold": require("../assets/fonts/Manrope-ExtraBold.ttf"),
+    "Manrope-Light": require("../assets/fonts/Manrope-Light.ttf"),
+    "Manrope-ExtraLight": require("../assets/fonts/Manrope-ExtraLight.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-      </Stack>
+      <Stack screenOptions={{ headerShown: false }} />
     </SafeAreaProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "manrope-regular": ["Manrope", "sans-serif"],
+        "manrope-medium": ["Manrope-Medium", "sans-serif"],
+        "manrope-semibold": ["Manrope-SemiBold", "sans-serif"],
+        "manrope-bold": ["Manrope-Bold", "sans-serif"],
+        "manrope-extrabold": ["Manrope-ExtraBold", "sans-serif"],
+        "manrope-light": ["Manrope-Light", "sans-serif"],
+        "manrope-extralight": ["Manrope-ExtraLight", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#6AB0E30A",
+          200: "#6AB0E31A",
+          300: "#6AB0E3",
+        },
+      },
+    },
   },
   plugins: [],
 };
