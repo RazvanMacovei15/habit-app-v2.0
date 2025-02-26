a67682bbf45e8943a330d6c55686d752f57011c9
razvanmc15 <razvanmc15@gmail.com>
2025-02-26 07:21:24 +0200

    another 1% progress..

--- components/DayByDayNavigation.tsx
+++ components/DayByDayNavigation.tsx
@@ -1,14 +1,52 @@
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { format } from "date-fns";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  format,
+  getISOWeek,
+  addDays,
+  subDays,
+  startOfWeek,
+  startOfISOWeek,
+  isLeapYear,
+  setISOWeek,
+  getYear,
+} from "date-fns";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 const DAYS_OF_WEEK = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 const DayByDayNavigation = () => {
   const today = new Date();
+  const year = today.getFullYear();
   const [date, setDate] = useState(today);
+
+  const thisWeek = getISOWeek(today);
+  const [yearWeek, setYearWeek] = useState(thisWeek);
+
+  const getMondayOfWeek = (year: number, weekNumber: number) => {
+    const fourthJanuary = new Date(year, 0, 4);
+    const referenceDate = setISOWeek(fourthJanuary, weekNumber);
+    console.log("Reference Date:", format(referenceDate, "EEEE, dd/MM/yyyy"));
+
+    return startOfWeek(referenceDate, { weekStartsOn: 1 });
+  };
+
+  const addWeek = () => {
+    const nextWeekNumber = getISOWeek(addDays(today, 7));
+    console.log("This Week Number:", thisWeek);
+    console.log("Next Week Number:", nextWeekNumber);
+    const year = getYear(today);
+
+    const nextWeekMonday = getMondayOfWeek(year, nextWeekNumber);
+
+    console.log(
+      "Next Week Monday:",
+      format(nextWeekMonday, "EEEE, dd/MM/yyyy")
+    );
+  };
+
   const formattedDate = format(date, "dd/MM");
-  const dayOfWeek = format(today, "EEE").toLowerCase();
+  const todayLC = format(today, "EEE").toLowerCase();
 
   return (
     <View className="flex flex-row items-center justify-around w-full rounded-b-xl my-2 gap-2 px-2">
@@ -24,6 +62,12 @@ const DayByDayNavigation = () => {
         >
           <Text>{day}</Text>
           <Text>{formattedDate}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              addWeek();
+            }}
+            className="bg-green-500 w-8 h-8"
+          />
         </View>
       ))}
     </View>
